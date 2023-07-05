1241ed47ae51d411dff7c10deda0e0b1a9e06942
ryumoon88 <naufalhady08@yahoo.com>
2023-07-05 23:18:47 +0700

    Add user and role seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,5 +15,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->create(['name' => 'Naufal Hady', 'email' => 'naufalhady08@yahoo.com', 'username' => 'naufalhady']);
     }
 }
